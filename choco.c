2ab64a6b5e6cb3f0d9e9baf8c497c8263c52ffb7
vijayakannamaneni <101324640+vijayakannamaneni@users.noreply.github.com>
2022-10-07 12:34:31 +0530

    Add files via upload

---
+++ choco.c
@@ -0,0 +1,31 @@
+//chocolates
+#include<stdio.h>
+  int chocolate(int arr[],int n)
+{
+  int p=0;
+  int q=n-1;
+  for(int i=0;i<n;i++)
+   {
+      if(p==q)
+      {
+        return arr[p];
+      }
+     else if(arr[p]>arr[q])
+      {
+        p++;
+      }
+     else
+     {
+       q--;
+    }
+}
+}
+int main()
+{
+  int n=5;
+   int arr[]={4,3,1,2,8};
+   int b=chocolate(arr,n);
+   printf("%d",b);
+}
+
+
